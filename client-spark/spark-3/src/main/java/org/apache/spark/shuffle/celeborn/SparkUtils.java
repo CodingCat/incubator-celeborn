a5131351f1f2a99f4d3f249c43016c8ed6378168
CodingCat <zhunansjtu@gmail.com>
2025-04-04 09:32:18 -0700

    addr comments1

--- client-spark/spark-3/src/main/java/org/apache/spark/shuffle/celeborn/SparkUtils.java
+++ client-spark/spark-3/src/main/java/org/apache/spark/shuffle/celeborn/SparkUtils.java
@@ -621,4 +621,10 @@ public class SparkUtils {
     FailedShuffleCleaner.setLifecycleManager(lifecycleManager);
     FailedShuffleCleaner.addShuffleIdReferringStage(celebornShuffeId, appShuffleIdentifier);
   }
+
+  public static void removeCleanedShuffleId(
+      LifecycleManager lifecycleManager, int celebornShuffleId) {
+    FailedShuffleCleaner.setLifecycleManager(lifecycleManager);
+    FailedShuffleCleaner.removeCleanedShuffleId(celebornShuffleId);
+  }
 }
--- client/src/main/scala/org/apache/celeborn/client/LifecycleManager.scala
+++ client/src/main/scala/org/apache/celeborn/client/LifecycleManager.scala
@@ -1159,6 +1159,7 @@ class LifecycleManager(val appUniqueId: String, val conf: CelebornConf) extends 
           shuffleIds.values.map {
             case (shuffleId, _) =>
               unregisterShuffle(shuffleId)
+              unregisterShuffleCallback.foreach(c => c.accept(shuffleId))
           })
       }
     } else {
@@ -1859,6 +1860,11 @@ class LifecycleManager(val appUniqueId: String, val conf: CelebornConf) extends 
   @volatile private var getShuffleIdForReaderCallback: Option[BiConsumer[Integer, String]] = None
   def registerGetShuffleIdForReaderCallback(callback: BiConsumer[Integer, String]): Unit = {
     getShuffleIdForReaderCallback = Some(callback)
+  }
+
+  @volatile private var unregisterShuffleCallback: Option[Consumer[Integer]] = None
+  def registerUnregisterShuffleCallback(callback: Consumer[Integer]): Unit = {
+    unregisterShuffleCallback = Some(callback)
   }
 
   def registerAppShuffleDeterminate(appShuffleId: Int, determinate: Boolean): Unit = {
